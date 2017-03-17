c314ef6f6576a9e978e9672ed0e1424698a8a5c5
Diana <diana.cm08@gmail.com>
2017-03-17 15:52:05 -0600

    Conectar a PostgreSQL

--- App.py
+++ App.py
@@ -1,10 +1,25 @@
 from flask import Flask, request, json, Response
+import psycopg2
 
 __author__ = 'Carlos Perez', 'Diana Camacho', 'Hillary Brenes'
 
-# Inicialización de Flask
+
 app = Flask(__name__)
+
+
+ # Conexión
+conexion = "host='localhost' dbname='MediosTransporte' user='administrador' password='admin'"
+
+print("conectando...\n	->%s" % (conexion))
+
+# Realizar la conexión
+conn = psycopg2.connect(conexion)
+
+  #para consultas a BD
+  #  cursor = conn.cursor()
+   # print ("Connected!\n")
 
 
 if __name__ == '__main__':
     app.run(port=8000, host='0.0.0.0')
+
