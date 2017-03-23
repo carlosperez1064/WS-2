73908c37978f410d697782885ccc263d12d19c2a
carlosperez1064 <carlosperez1064@gmail.com>
2017-03-23 16:36:25 -0600

    Avance Jueves
    Algoritmo para origen, destino y vecino (avion y tren)

--- App.py
+++ App.py
@@ -5,132 +5,160 @@ import matplotlib.pyplot as plt
 
 __author__ = 'Carlos Perez', 'Diana Camacho', 'Hillary Brenes'
 
-
 app = Flask(__name__)
 
-mapa = nx.Graph() #Crear el grafo
+mapa = nx.Graph()  # Crear el grafo
+
 
 def conectarBaseDatos():
-
     conexion = "host='localhost' dbname='MediosTransporte' user='administrador' password='admin'"
     print("conectando...\n	->%s" % (conexion))
 
-    conn = psycopg2.connect(conexion) # Realizar la conexión a DB
+    conn = psycopg2.connect(conexion)  # Realizar la conexión a DB
+
 
 def GrafoMapa():
-
-#Agregar nodos al grafo con atributos
-    mapa.add_node(1, {"Bus" : True, "Taxi" : True, "Tren" : False, "Avion" :False})
-    mapa.add_node(2, {"Bus" : True, "Taxi" : True, "Tren" : False, "Avion" :False})
-    mapa.add_node(3, {"Bus" : True, "Taxi" : True, "Tren" : False, "Avion" :False})
-    mapa.add_node(4, {"Bus" : True, "Taxi" : True, "Tren" : False, "Avion" :True})
-    mapa.add_node(5, {"Bus" : True, "Taxi" : True, "Tren" : False, "Avion" :True})
-    mapa.add_node(6, {"Bus" : True, "Taxi" : True, "Tren" : False, "Avion" :False})
-    mapa.add_node(7, {"Bus" : True, "Taxi" : True, "Tren" : True, "Avion" :True})
-    mapa.add_node(8, {"Bus" : True, "Taxi" : True, "Tren" : False, "Avion" :False})
-    mapa.add_node(9, {"Bus" : True, "Taxi" : True, "Tren" : True, "Avion" :False})
-    mapa.add_node(10, {"Bus" : True, "Taxi" : True, "Tren" : False, "Avion" :False})
-    mapa.add_node(11, {"Bus" : True, "Taxi" : True, "Tren" : False, "Avion" :False})
-    mapa.add_node(12, {"Bus" : True, "Taxi" : True, "Tren" : True, "Avion" :False})
-    mapa.add_node(13, {"Bus" : True, "Taxi" : True, "Tren" : True, "Avion" :False})
-    mapa.add_node(14, {"Bus" : True, "Taxi" : True, "Tren" : False, "Avion" :False})
-    mapa.add_node(15, {"Bus" : True, "Taxi" : True, "Tren" : True, "Avion" :False})
-    mapa.add_node(16, {"Bus" : True, "Taxi" : True, "Tren" : False, "Avion" :False})
-    mapa.add_node(17, {"Bus" : True, "Taxi" : True, "Tren" : False, "Avion" :False})
-    mapa.add_node(18, {"Bus" : True, "Taxi" : True, "Tren" : False, "Avion" :False})
-    mapa.add_node(19, {"Bus" : True, "Taxi" : True, "Tren" : False, "Avion" :True})
-    mapa.add_node(20, {"Bus" : True, "Taxi" : True, "Tren" : False, "Avion" :False})
-    mapa.add_node(21, {"Bus" : True, "Taxi" : True, "Tren" : False, "Avion" :True})
-    mapa.add_node(22, {"Bus" : True, "Taxi" : True, "Tren" : False, "Avion" :False})
-    mapa.add_node(23, {"Bus" : True, "Taxi" : True, "Tren" : True, "Avion" :False})
-    mapa.add_node(24, {"Bus" : True, "Taxi" : True, "Tren" : False, "Avion" :False})
+    # Agregar nodos al grafo con atributos
+    mapa.add_node(1, {"Nombre": "Volcan Arenal", "Bus": True, "Taxi": True, "Tren": False, "Avion": False})
+    mapa.add_node(2, {"Nombre": "Quepos", "Bus": True, "Taxi": True, "Tren": False, "Avion": False})
+    mapa.add_node(3, {"Nombre": "Las Juntas", "Bus": True, "Taxi": True, "Tren": False, "Avion": False})
+    mapa.add_node(4, {"Nombre": "Cariari Pococi", "Bus": True, "Taxi": True, "Tren": False, "Avion": True})
+    mapa.add_node(5, {"Nombre": "Puerto Jimenez", "Bus": True, "Taxi": True, "Tren": False, "Avion": True})
+    mapa.add_node(6, {"Nombre": "Volcan Rincon de la Vieja", "Bus": True, "Taxi": True, "Tren": False, "Avion": False})
+    mapa.add_node(7, {"Nombre": "Volcan Poas", "Bus": True, "Taxi": True, "Tren": True, "Avion": True})
+    mapa.add_node(8, {"Nombre": "Upala", "Bus": True, "Taxi": True, "Tren": False, "Avion": False})
+    mapa.add_node(9, {"Nombre": "Puerto Viejo Sarapiqui", "Bus": True, "Taxi": True, "Tren": True, "Avion": False})
+    mapa.add_node(10, {"Nombre": "Cahuita", "Bus": True, "Taxi": True, "Tren": False, "Avion": False})
+    mapa.add_node(11, {"Nombre": "Filadelfia", "Bus": True, "Taxi": True, "Tren": True, "Avion": False})
+    mapa.add_node(12, {"Nombre": "Volcan Turrialba", "Bus": True, "Taxi": True, "Tren": True, "Avion": False})
+    mapa.add_node(13, {"Nombre": "San isidro del General", "Bus": True, "Taxi": True, "Tren": True, "Avion": False})
+    mapa.add_node(14, {"Nombre": "Uvita", "Bus": True, "Taxi": True, "Tren": False, "Avion": False})
+    mapa.add_node(15, {"Nombre": "Volcan Irazu", "Bus": True, "Taxi": True, "Tren": True, "Avion": False})
+    mapa.add_node(16, {"Nombre": "Volcan Tenorio", "Bus": True, "Taxi": True, "Tren": False, "Avion": False})
+    mapa.add_node(17, {"Nombre": "Moravia", "Bus": True, "Taxi": True, "Tren": False, "Avion": False})
+    mapa.add_node(18, {"Nombre": "Cerro Chirripo", "Bus": True, "Taxi": True, "Tren": False, "Avion": False})
+    mapa.add_node(19, {"Nombre": "La Casona Santa Rosa", "Bus": True, "Taxi": True, "Tren": False, "Avion": True})
+    mapa.add_node(20, {"Nombre": "Bribri", "Bus": True, "Taxi": True, "Tren": False, "Avion": False})
+    mapa.add_node(21, {"Nombre": "Puerto Viejo Talamanca", "Bus": True, "Taxi": True, "Tren": False, "Avion": True})
+    mapa.add_node(22, {"Nombre": "Los Chiles", "Bus": True, "Taxi": True, "Tren": False, "Avion": False})
+    mapa.add_node(23, {"Nombre": "Volcan Barva", "Bus": True, "Taxi": True, "Tren": True, "Avion": False})
+    mapa.add_node(24, {"Nombre": "Santa Cruz", "Bus": True, "Taxi": True, "Tren": False, "Avion": False})
     print(mapa.nodes(data=True))
 
     # Lista de edges y pesos que será insertada (nodo origen, nodo destino, distancia en km)
-    lista = [(19, 24, 148),
-             (19, 6, 52),
+    lista = [(19, 24, 60),
+             (19, 6, 40),
+             (19, 11, 50),
              (24, 11, 74),
              (11, 6, 92),
              (11, 3, 112),
              (6, 8, 69),
-             (6, 22, 235),
+             (6, 22, 60),
              (8, 16, 46),
-             (22, 16, 179),
-             (16, 9, 178),
+             (22, 16, 70),
+             (16, 9, 120),
              (16, 1, 69),
-             (3, 1, 104),
-             (3, 7, 160),
-             (1, 9, 113),
+             (3, 1, 30),
+             (3, 7, 60),
+             (1, 9, 90),
              (7, 23, 43),
-             (7, 2, 187),
-             (7, 15, 103),
-             (9, 23, 97),
+             (7, 2, 107),
+             (7, 15, 40),
+             (9, 23, 50),
              (9, 4, 87),
-             (2, 13, 76),
+             (2, 13, 60),
              (2, 14, 61),
-             (15, 23, 82),
-             (15, 12, 19),
-             (15, 13, 145),
-             (23, 4, 121),
-             (4, 10, 177),
-             (4, 12, 156),
-             (14, 18, 111),
-             (14, 5, 149),
-             (14, 13, 54),
-             (13, 18, 60),
-             (12, 18, 114),
+             (15, 23, 30),
+             (15, 12, 10),
+             (15, 13, 32),
+             (23, 4, 70),
+             (4, 10, 120),
+             (4, 12, 80),
+             (14, 18, 70),
+             (14, 5, 90),
+             (14, 13, 55),
+             (13, 18, 20),
+             (12, 18, 60),
              (12, 17, 61),
-             (5, 20, 591),
-             (18, 20, 274),
-             (18, 17, 94),
-             (17, 10, 190),
+             (5, 20, 180),
+             (18, 20, 50),
+             (18, 17, 40),
+             (17, 10, 20),
              (10, 21, 16),
              (20, 21, 13),
-             (19, 4, 364),
-             (19, 5, 505),
+             (19, 4, 300),
+             (19, 5, 500),
              (19, 7, 275),
-             (24, 4, 286),
-             (24, 5, 426),
-             (24, 7, 196),
-             (5, 4, 480),
-             (5, 7, 391)]
+             (24, 4, 260),
+             (24, 5, 420),
+             (24, 7, 190),
+             (5, 4, 390),
+             (5, 7, 400)]
 
-    mapa.add_weighted_edges_from(lista) #Agregar los bordes con sus respectivos pesos
-    nx.draw_networkx(mapa,with_labels=True) #Dibujar rutas del mapa (nodos conectados)
+    mapa.add_weighted_edges_from(lista)  # Agregar los bordes con sus respectivos pesos
+    nx.draw_networkx(mapa, with_labels=True)  # Dibujar rutas del mapa (nodos conectados)
     plt.show()
+
+
+def nombres(param):
+    for nodo in mapa.node:
+        if nodo == param:
+            return (mapa.node[nodo]["Nombre"])
+
+
+def medios(nodo1, nodo2, medio):
+    if mapa.node[nodo1][medio] and mapa.node[nodo2][medio]:
+        return True
+
 
 @app.route('/api/viajando/consultas', methods=['POST'])
 def consultas():
+    # in_args = request.args  # Obtener todos los parámetros
 
-    in_args = request.args  # Obtener todos los parámetros
-
-    origen = in_args['origen'] #Seleccionar parametro con clave origen
-    destino = in_args['destino'] #Seleccionar parametro con clave destino
-    tipoTransporte = in_args['tipoTransporte'] #Seleccionar parametro con clave tipoTransporte
+    # origen = in_args['origen'] #Seleccionar parametro con clave origen
+    # destino = in_args['destino'] #Seleccionar parametro con clave destino
+    # tipoTransporte = in_args['tipoTransporte'] #Seleccionar parametro con clave tipoTransporte
+    origen = 19
+    destino = 10
+    tipoTransporte = 'Avion'
+    vecinosO = mapa.neighbors(origen)
+    vecinosD = mapa.neighbors(destino)
 
     # Determinar Rutas más cortas
-    Grafo = nx.dijkstra_path(mapa, origen,destino) #Tomar parametros para determinar ruta corta (usa algoritmo Dijkstra)
-    #plt.savefig("path.png")
-    print (Grafo)
+    # Grafo = nx.dijkstra_path(mapa, origen,destino) #Tomar parametros para determinar ruta corta (usa algoritmo Dijkstra)
+    # plt.savefig("path.png")
+    # print (Grafo)
 
-    #Determinar Rutas vecinas
-    if mapa.node[origen][tipoTransporte] and mapa.node[destino][tipoTransporte]:
-        print ("Viaje directo")
-    else:
-        vecinos= mapa.neighbors(destino)
-        print (vecinos)
-        for vecino in vecinos:
-            if mapa.node[vecino][tipoTransporte]:
-                print ("Viaje a ", vecino, " y luego en otro medio de transporte disponible")
+    # Determinar si en los nodos vecinos al destino final, existe un medio de transporte más rápido (avión o tren)
+    # en cuyo caso, enviaría a la persona hasta ese nodo en cualquiera de esos dos medios de transporte, y luego al
+    # nodo de destino final en bus o en taxi.
 
-    #elif mapa.node[origen]['Avion'] and mapa.neighbors(destino)[]
+    if tipoTransporte == 'Avion' or tipoTransporte == 'Tren':
+        if mapa.node[origen][tipoTransporte] and mapa.node[destino][tipoTransporte]:
+            print("Viaje directo de ", nombres(origen), "a", nombres(destino), "en", tipoTransporte)
+        else:
+            if mapa.node[origen][tipoTransporte]:
+                for vecino in vecinosD:
+                    if mapa.node[vecino][tipoTransporte]:
+                        print("Viaje de", origen, "a", vecino, "en", tipoTransporte, "y luego a", destino,
+                              "en bus o taxi")
+            elif mapa.node[destino][tipoTransporte]:
+                for vecino in vecinosO:
+                    if mapa.node[vecino][tipoTransporte]:
+                        print("Viaje de", origen, "a", vecino, "en bus o taxi", "y luego de", vecino, "a", destino,
+                              "en avion")
+            else:
+                
 
-#if __name__ == '__main__':
-    #app.run(port=8000, host='0.0.0.0')
 
-#conectarBaseDatos()
+# if __name__ == '__main__':
+# app.run(port=8000, host='0.0.0.0')
+
+
+# conectarBaseDatos()
 GrafoMapa()
-#consultas()
+consultas()
+# nombres()
 
-#CREATE TABLE tren (id integer, data json);
-#INSERT INTO tren VALUES (12,'{"NombreCompania": "Inconfer","Ruta": {"Origen": "San Jose","Destino": "Cartago","Horario": "L-D"}}');
+# CREATE TABLE tren (id integer, data json);
+# INSERT INTO tren VALUES (12,'{"NombreCompania": "Inconfer","Ruta": {"Origen": "San Jose","Destino": "Cartago","Horario": "L-D"}}');
