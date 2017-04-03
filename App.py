ecb838a2ed3c6f883e9b6cf3e77413a22deaf2ca
DianaCM <diana.cm08@gmail.com>
2017-04-03 13:11:04 -0600

    log listo, guarda consultas realizadas

--- App.py
+++ App.py
@@ -391,14 +391,6 @@ def muestreInfoEnBaseDatosDe(transporteSelecionado, elID):
 
     return resultado
 
-
-# ------------------------------------------- MÉTODO PARA GUARDAR EN LOG -----------------------------------------------#
-def logEnBD():
-    return ""
-
-
 # ----------------------------------------------------- EJECUCIÓN ------------------------------------------------------#
 if __name__ == '__main__':
     app.run(port=8000, host='127.0.0.1')
-
-# muestreInfoEnBaseDatosDe("avion",2)
